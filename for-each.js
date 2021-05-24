a990a92f11d3bf6556ba6ba55cf875d60c4e4ced
Danny Cairns <dpcairns@gmail.com>
2021-05-24 13:39:25 -0700

    Update for-each.js

--- for-each.js
+++ for-each.js
@@ -1,6 +1,16 @@
 // Use the forEach method to solve these problems! All functions should return a NEW array, without mutating the old array.
 
 /*
+Input:
+[
+    { name: 'spot', type: 'dog' },
+    { name: 'rover', type: 'dog' },
+    { name: 'jumpy', type: 'frog' },
+    { name: 'einstein', type: 'cat' },
+]
+*/
+
+/*
 Output:
 
 ['spot', 'rover', 'jumpy', 'einstein']
